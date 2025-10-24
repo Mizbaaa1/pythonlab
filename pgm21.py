9e9e436048a2db08c67c735d20ac950fc6e64f09
Mizbaaa1 <misbachettali@gmail.com>
2025-10-24 10:37:57 +0530

    done

---
+++ pgm21.py
@@ -0,0 +1,6 @@
+lst = list(map(int,input("enter number:").split()))
+s = 0
+for i in lst:
+    s += i
+print("sum:",s)
+
---
+++ pgm22.py
@@ -0,0 +1,4 @@
+a,b = map(int,input("enter range: ").split())
+res = [x for x in range(a, b+1) if int(x**0.5)**2 ==x  and  all(int(d) % 2 == 0 for d in str(x))]
+print(res)          
+
